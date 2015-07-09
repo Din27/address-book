54e424b2e3e32d1676e9184898d2961d6af6bfbd
Din <xenon027@gmail.com>
2015-07-10 00:10:21 +0300

    Add logging

--- src/main/java/com/chelyadin/test/address_book/web/MainController.java
+++ src/main/java/com/chelyadin/test/address_book/web/MainController.java
@@ -1,5 +1,7 @@
 package com.chelyadin.test.address_book.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("/")
     public String index() {
+        LOG.info("Index page request");
         return "index";
     }
 
